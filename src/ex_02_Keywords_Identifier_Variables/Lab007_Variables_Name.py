ae8db269b6c0a34fea0fb37820af4fe14229f417
Ruma <rumaghosh2006@gmail.com>
2025-10-12 22:41:22 +0530

    11th october class

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab007_Variables_Name.py
@@ -0,0 +1,17 @@
+#123abc = 90
+_ruma = "amit"
+_abc = 23
+pi = 3.14
+name = "Ruma"
+isMale = True
+
+# Type function
+print(type(_ruma)) # <class 'str'>
+print(type(pi)) # <class 'float'>
+print(type(isMale)) #<class 'bool'>
+
+complex_number = 2+3j
+print(complex_number)
+print(complex_number.real)
+print(complex_number.imag)
+print(type(complex_number))
---
+++ src/ex_02_Keywords_Identifier_Variables/Lab008_Dynamically_typed.py
@@ -0,0 +1,6 @@
+#Dynamically typed
+
+age=98
+print(type(age))
+age="ruma"
+print(type(age))
