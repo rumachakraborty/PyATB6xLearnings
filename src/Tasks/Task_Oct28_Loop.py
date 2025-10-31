e88edbd51c589f9546b65461a2f1f9781907c0d4
Ruma <rumaghosh2006@gmail.com>
2025-10-31 07:44:03 +0530

    30 october class for Lab all task completed

---
+++ src/Tasks/Task_Oct28_Loop.py
@@ -0,0 +1,18 @@
+"""
+Print even numbers between 1 and 20
+i%2 == 0 -> even
+# """
+# print("Print even numbers between 1 and 20")
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+print("----------------------------------------------")
+"""
+Print numbers from 10 down to 1
+step in the for -negative
+"""
+print("Print even numbers between 10 and 1")
+for i in range(10, 0, -1):
+     if i % 2 == 0:
+        print(i)
+print("----------------------------------------------")
---
+++ src/Tasks/Task_Oct28_Loop1.py
@@ -0,0 +1,8 @@
+"""
+Skip numbers divisible by 3, from (0,100)
+# """
+for i in range(1, 30):
+    if i % 3 == 0:
+        continue
+    else:
+        print(i)
